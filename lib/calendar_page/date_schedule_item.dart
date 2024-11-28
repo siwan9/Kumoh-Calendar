6f92355efffd896fe3e03bfa08c9d0f473ab784c
wei756 <wei756fg@gmail.com>
2024-11-28 11:22:30 +0900

    feat: schedule data 객체 정의, 일정 객체랑 ui 연결

--- lib/calendar_page/date_schedule_item.dart
+++ lib/calendar_page/date_schedule_item.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:kumoh_calendar/data/schedule_data.dart';
 
 class ScheduleItemWidget extends StatelessWidget {
-  const ScheduleItemWidget({super.key, required this.name});
+  const ScheduleItemWidget({super.key, required this.schedule});
 
-  final String name;
+  final ScheduleData schedule;
 
   @override
   Widget build(BuildContext context) {
@@ -13,12 +14,13 @@ class ScheduleItemWidget extends StatelessWidget {
         backgroundColor: WidgetStateProperty.all(Colors.blue),
         alignment: Alignment.centerLeft,
         minimumSize: WidgetStateProperty.all(const Size(0, 24)),
-        padding: WidgetStateProperty.all(const EdgeInsets.symmetric(horizontal: 4)),
+        padding:
+            WidgetStateProperty.all(const EdgeInsets.symmetric(horizontal: 4)),
         shape: WidgetStateProperty.all(RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(4),
         )),
       ),
-      child: Text(style: const TextStyle(color: Colors.white), name),
+      child: Text(style: const TextStyle(color: Colors.white), schedule.name),
     );
   }
 }
